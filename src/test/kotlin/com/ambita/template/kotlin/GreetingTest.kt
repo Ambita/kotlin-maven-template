b15ffb15c745585b001111a338c861b5bc54cc5e
Bjorn Hamre <bha@ambita.com>
2019-02-06 16:16:57 +0100

    Initial setup

---
+++ src/test/kotlin/com/ambita/template/kotlin/GreetingTest.kt
@@ -0,0 +1,30 @@
+package com.ambita.template.kotlin
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+/**
+ * Greeting test case.
+ */
+class GreetingTest {
+
+    /**
+     * Given the user name is "Kotlin"
+     * When we call "sayHai" with user name.
+     * Then it should return "Hello Kotlin!"
+     */
+    @Test
+    fun `say hi with parameter`() {
+        assertEquals("Hello Kotlin!", sayHi("Kotlin"))
+    }
+
+    /**
+     * Given the user name is not specified
+     * When we call "sayHai" without any parameter.
+     * Then it should return "Hello World!"
+     */
+    @Test
+    fun sayHiWithoutParameter() {
+        assertEquals("Hello World!", sayHi())
+    }
+}
